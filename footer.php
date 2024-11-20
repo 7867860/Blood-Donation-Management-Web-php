604c0e9f26f7742a5d20d5472f8f7243f121683c
Didar Ahmad <70826779+7867860@users.noreply.github.com>
2024-11-20 09:19:56 -0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+  <style>
+  #footer {
+
+  position:absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 75px;
+  background-color:#000000;
+  color:white;
+  text-align: center;
+}
+  </style>
+</head>
+<body>
+  <div id="footer" >
+  <b><center>Thankyou  for Visiting <br>
+  Blood Bank & Donation Management
+  <br>
+  Help Us More By Sharing With Your Family&Friends.
+  </center>
+  </div>
+
+
+</body>
+
+</html>
